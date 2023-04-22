eae333bb93d00b285b929d5853490cafbe205c12
mohammad <mohammad.khalifa@haw-hamburg.de>
2023-04-22 23:54:35 +0200

    test_für_gitHub

--- CaDS_RMI_Example/startProviderWithSimulation.sh
+++ CaDS_RMI_Example/startProviderWithSimulation.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+#java --module-path %PATH_TO_FX% --add-modules javafx.control
 java -cp CaDSRMI.jar org.cads.vs.roboticArm.rmi.provider.CaDSRMIProvider -h 127.0.0.1 -S true -p 8888 -ID rmiExampleSimulation
